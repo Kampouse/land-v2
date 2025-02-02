b55a81ec107c30f9332a20e1bcc67fa9aa1a5cc4
jean-philippe <jp@zelexio.com>
2025-02-01 19:40:12 -0500

    that good

--- src/components/picture.tsx
+++ src/components/picture.tsx
@@ -1,6 +1,4 @@
 import { component$, useStyles$ } from "@builder.io/qwik";
-import { GithubLinkRow } from "./content";
-import { XLinkRow } from "./content";
 import * as   icon from "lucide-qwik"
 
 export const ProfilePicture = component$(() => {
@@ -24,16 +22,14 @@ export const ProfilePicture = component$(() => {
         height={150}
       />
       <div class="absolute inset-0 w-full h-full scale-100 sm:scale-[1.5] md:scale-[2]">
-        <div class="absolute md:top-8 left-1/2 -translate-x-1/2 -translate-y-16 sm:-translate-y-14 md:-translate-y-12 animate-[float_3s_ease-in-out_infinite]">
-          <icon.GithubIcon class="w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 text-xy-green-accent" />
-        </div>
 
-        <a href="https://www.linkedin.com/in/jemartel/" target="_blank" class="absolute left-0 top-1/2 -translate-x-12 sm:-translate-x-14 md:-translate-x-16 -translate-y-1/2 animate-[float_3s_ease-in-out_infinite_1s] hover:text-xy-green-accent transition-colors">
+        <a href="https://www.linkedin.com/in/jemartel/" target="_blank" class="absolute cursor-pointer left-0 top-1/2 -translate-x-16 sm:-translate-x-14 md:-translate-x-5 -translate-y-1/2 animate-[float_3s_ease-in-out_infinite_1s] hover:text-xy-green-accent transition-colors">
           <icon.LinkedinIcon class="w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 text-xy-green-accent" />
         </a>
-        <div class="absolute right-0 top-1/2 translate-x-12 sm:translate-x-14 md:translate-x-16 -translate-y-1/2 animate-[float_3s_ease-in-out_infinite_1.5s]">
-          <icon.XIcon class="w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 text-xy-green-accent" />
-        </div>
+        <a href="https://github.com/Kampouse" target="_blank" class="absolute cursor-pointer right-0 top-1/2 translate-x-16 sm:translate-x-0 md:translate-x-5 -translate-y-1/2 animate-[float_3s_ease-in-out_infinite_1.5s] hover:text-xy-green-accent transition-colors">
+          <icon.GithubIcon class="w-8 h-8 sm:w-6 sm:h-6 md:w-8 md:h-8 text-xy-green-accent" />
+        </a>
+
       </div>
     </div>
   )
